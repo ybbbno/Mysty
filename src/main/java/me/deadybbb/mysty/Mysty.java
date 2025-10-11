d436cd70638104ae90bdd9c8064c84eee1f5fecd
mitsu-wq <bmitsu779@gmail.com>
2025-10-11 07:53:47 +0700

    Update libs and add listener for controlling mob spawning in zone

--- src/main/java/me/deadybbb/mysty/Mysty.java
+++ src/main/java/me/deadybbb/mysty/Mysty.java
@@ -3,6 +3,7 @@ package me.deadybbb.mysty;
 import me.deadybbb.customzones.listeners.ZoneListenerRegistry;
 import me.deadybbb.mysty.clocknoise.ClockNoiseListener;
 import me.deadybbb.mysty.compasnoise.CompassNoiseListener;
+import me.deadybbb.mysty.mobcontrol.MobControlListener;
 import me.deadybbb.ybmj.PluginProvider;
 
 public final class Mysty extends PluginProvider {
@@ -11,6 +12,7 @@ public final class Mysty extends PluginProvider {
     public void onEnable() {
         ZoneListenerRegistry.registerListener(this, new CompassNoiseListener(this));
         ZoneListenerRegistry.registerListener(this, new ClockNoiseListener(this));
+        ZoneListenerRegistry.registerListener(this, new MobControlListener());
     }
 
     @Override
